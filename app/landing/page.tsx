43441e890b028def8e74d95ad091d11ef7958672
Kolton Thompson <mr.kolton@gmail.com>
2023-09-13 13:44:51 -0600

    refactor: sliced first index out of variable to array map in order to maintain design

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -33,6 +33,8 @@ export default function Landing() {
     },
   ]
 
+  const pagesToMap = pages.slice(1)
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white font-sans">
       <div className="w-full max-w-screen-lg text-center">
@@ -57,21 +59,32 @@ export default function Landing() {
         </header>
         {/* Main Content */}
         <main className="container mx-auto px-4 py-8 text-center">
-          {pages.map((page) => {
-            return (
-              <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <div
-                  id={page.slug}
-                  className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out"
-                >
-                  <h3 className="text-xl font-semibold mb-2 text-slate-800">
-                    {page.title}
-                  </h3>
-                  <p className="text-lg text-slate-800">{page.title}</p>
-                </div>
-              </section>
-            )
-          })}
+          <section className="mb-8">
+            <h2 className="text-2xl font-semibold mb-4">Featured Content</h2>
+            <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
+              {/* Featured content or promotional banners */}
+              <h3 className="text-xl font-semibold mb-2 text-slate-800">
+                Start a League
+              </h3>
+              <p className="text-lg text-slate-800">
+                Build your team, make strategic picks, and dominate the
+                leaderboard.
+              </p>
+            </div>
+          </section>
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {pagesToMap.map((page) => (
+              <div
+                key={page.slug}
+                className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out"
+              >
+                <h3 className="text-xl font-semibold mb-2 text-slate-800">
+                  {page.title}
+                </h3>
+                <p className="text-lg text-slate-800">{page.description}</p>
+              </div>
+            ))}
+          </section>
         </main>
         {/* Footer */}
         <footer className="bg-gray-800 p-4 w-full">
@@ -83,65 +96,3 @@ export default function Landing() {
     </div>
   )
 }
-
-//  <section className="mb-8">
-//             <h2 className="text-2xl font-semibold mb-4">Featured Content</h2>
-//             <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
-//               {/* Featured content or promotional banners */}
-//               <h3 className="text-xl font-semibold mb-2 text-slate-800">
-//                 Start a League
-//               </h3>
-//               <p className="text-lg text-slate-800">
-//                 Build your team, make strategic picks, and dominate the
-//                 leaderboard.
-//               </p>
-//             </div>
-//           </section>
-//           <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
-//             <div>
-//               {/* Featured League 1 */}
-//               <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
-//                 <h3 className="text-xl font-semibold mb-2 text-slate-800">
-//                   PGA Masters League
-//                 </h3>
-//                 <p className="text-lg text-slate-800">
-//                   Join this prestigious league and compete against the best.
-//                   Prizes await the top performers!
-//                 </p>
-//               </div>
-//             </div>
-//             <div>
-//               {/* Featured League 2 */}
-//               <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
-//                 <h3 className="text-xl font-semibold mb-2 text-slate-800">
-//                   Mock Draft
-//                 </h3>
-//                 <p className="text-lg text-slate-800">
-//                   New to Fantasy Golf? Try a Mock Draft and learn the ropes
-//                   while having fun.
-//                 </p>
-//               </div>
-//             </div>
-//             <div>
-//               {/* Featured League 2 */}
-//               <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
-//                 <h3 className="text-xl font-semibold mb-2 text-slate-800">
-//                   Upcoming Tournaments
-//                 </h3>
-//                 <p className="text-lg text-slate-800">
-//                   Insight to Players projections in upcoming events
-//                 </p>
-//               </div>
-//             </div>
-//             <div>
-//               {/* Featured League 2 */}
-//               <div className="bg-white p-6 rounded-lg shadow-lg hover:bg-violet-50 transition duration-300 ease-in-out">
-//                 <h3 className="text-xl font-semibold mb-2 text-slate-800">
-//                   World Rankings
-//                 </h3>
-//                 <p className="text-lg text-slate-800">
-//                   PGA World Rankings and Money Leaders
-//                 </p>
-//               </div>
-//             </div>
-//           </section>
